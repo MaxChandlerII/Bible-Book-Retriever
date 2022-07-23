2f17dab118f931c3f3411311df098c08798133eb
MaxChandlerII <mfchand2@ncsu.edu>
2022-07-23 07:19:24 -0400

    F( )'s made, lots restructure. WIP still.

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ from sqlalchemy import false, true #I didn't add this. Python must've added for 
 
 #Word Doc Variables
 doc = docx.Document("C:\Personal_Repos\Bible-Book-Retriever\Proverbs.docx")
-proverbs_list = [ ] #A blank 2-D list. Zero index accesses a Chapter. 1st index accesses a Verse in the specified Chapter
-
-
+pr10_list = [ ] #A blank 2-D list. Zero index accesses a Chapter. 1st index accesses a Verse in the specified Chapter
+pr10_num_ver = 0 #will hold # verses found in Proverbs 10
 
 '''
 #Start of Google Search (make a F( ) )
@@ -30,16 +29,17 @@ print("Finished search.")
 '''
 
 def get_number_verses(b_print):
-    num_verses = doc.paragraphs
-    num_verses = len(num_verses)
+    pr10_num_ver = doc.paragraphs
+    pr10_num_ver = len(pr10_num_ver)
     if( true == b_print ):
-        print("Number of verses = ", num_verses )
-    return num_verses
+        print("Number of verses = ", pr10_num_ver )
+    return pr10_num_ver
 
 def print_all_verses():
     #Print each verse in the Terminal
-    for para in number_verses:
-        print(number_verses.text)
+    pr10_num_ver = get_number_verses(false)
+    for para in doc.paragraphs:
+        print(doc.text)
         print("-------")
 
 def get_all_verses():
@@ -50,9 +50,50 @@ def get_all_verses():
             # displaying the words           
             print(word) 
 
-print("hello Max")
-b_print = false
-number_verses = 0
-number_verses = get_number_verses( b_print )
-print("# of Verses = ", number_verses)
-get_all_verses()
+def getText( ):
+#def getText(filename):
+    #doc = docx.Document(filename)
+    #fullText = []
+    for para in doc.paragraphs:
+        #fullText.append(para.text)
+        pr10_list.append(para.text)
+    #return '\n'.join(fullText)
+
+'''
+Intended Design
+1) 2-D List, Zero index accesses a Chapter. 1st index accesses a Verse in the specified Chapter
+2) Search for every strong's and remove from List
+3) Find repeated words
+    A) Start w/first English word and store in local var
+    B) compare every word in that verse to it and every word in each subsequent verse and 
+    C) Keep track # of times repeated
+    D) remove the repeated word from the other verses? (to prevent redundant checking)
+    E) Repeated Words
+        - Keep a 2-D List w/repeated words, append words as detected. Store the word in index 0, store the verses in index 1.
+        - Keep a 2-D List w/non-repeated words, append words as detected. Store the word in index 0, store the verses in index 1.
+4) Print in terminal the repeated words and non-repeated words
+5) Store in word doc the repeated words
+6) Highlight repeated words. If "wisdom" is repeated 10 times in the chapter, highlight all occurances same color. Each word and all of its occurances get a unique color
+'''
+
+# Defining main function
+def main():
+    print("hello Max")
+    '''b_print = true
+    get_number_verses( b_print )
+    print_all_verses()'''
+    getText()
+    i = 0
+    for i in range(31):
+        '''proverbs_list[i] = doc.paragraphs[i]
+        print("proverbs_list[",i,"] = ",pr10_list[i] )'''
+        print("proverbs_list[",i,"] = ",pr10_list[i] )
+        #if( ) #pull the verse # out of proverbs list, then store the verse words in other column
+        #don't think i need a F( ) to traverse to get the length of pr10, i think there's a var in the doc or para var for len already
+
+  
+  
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
