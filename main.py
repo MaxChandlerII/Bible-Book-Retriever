90050e39f6b231670aa1d5e7db51fa9481a4ff95
MaxChandlerII <mfchand2@ncsu.edu>
2022-08-27 09:56:42 -0400

    Comment Google API out. Test word parsing and matching with a local txt of Genesis 1.

--- main.py
+++ main.py
@@ -16,12 +16,16 @@
 Recursively extracts the text from a Google Doc.
 """
 from __future__ import print_function
+import enum
+from posixpath import split
 
 import googleapiclient.discovery as discovery
 from google_OAuth import G_OAuth
+import pandas as pd
+import os, re
 
 DISCOVERY_DOC = 'https://docs.googleapis.com/$discovery/rest?version=v1'
-DOCUMENT_ID = '178vGY1f5Y4KXqjY1ZyUIp_pYXZZtJnCnZj47AO-K0WY'
+DOCUMENT_ID = '1MRqv0fppujBvQwmiYQ18InDG5S-RE5J0rGyfiY7zmE4'
 
 def read_paragraph_element(element):
     """Returns the text in the given ParagraphElement.
@@ -63,12 +67,52 @@ def read_structural_elements(elements):
 
 
 def main():
+    local_doc = 'local_doc.txt'
     """Uses the Docs API to print out the text of a document."""
+    '''
     http = G_OAuth.get_authorized_http()
     docs_service = discovery.build('docs', 'v1', http=http, discoveryServiceUrl=DISCOVERY_DOC)
     doc = docs_service.documents().get(documentId=DOCUMENT_ID).execute()
     doc_content = doc.get('body').get('content')
-    print(read_structural_elements(doc_content))
+    # print(read_structural_elements(doc_content))
+    text = read_structural_elements(doc_content)
+    # save it into a Local File
+    with open("local_doc.txt","w") as local_doc:
+        local_doc.write(text)
+    '''
+
+    # reload the Local File and convert it to a Pandas DataFrame
+    # with open('local_doc.txt', 'r') as f:
+    #         text = [line for line in f.readlines()]
+    # df = pd.DataFrame(text,columns=['text'])
+
+    word_to_match = 'God'
+    num_occurances = 0
+    gen1 = []
+
+    if not os.path.isfile(local_doc):
+        print('Local File does NOT exist.')
+    else:
+        '''
+        file    =   _io.TextIOWrapper
+        line    =   str
+        word    =   tuple
+        gen1    =   list
+        gen1[i] =   tuple
+        '''
+        with open('local_doc.txt','r') as file: 
+            for line in file:
+                for word in enumerate(line.split()):
+                    gen1.append(word)
+
+            print('length of gen1[] = %s' %str(len(gen1)) )
+
+            # for line in file:
+                # for word in enumerate( line.split() ):
+                    # print('length of gen1[] = %s' %str(len(gen1)) )
+                    # num_occurances = gen1.count( word )
+                    # print('length of gen1[] = %s' %str(len(gen1)) )
+                    # print("word = '{cword}' appears {times} in gen1[]".format(cword=word, times=str(num_occurances) ) ) 
 
 if __name__ == '__main__':
     main()
