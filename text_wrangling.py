bf7902ee1acd3561829a21de41001048c38f7071
MaxChandlerII <mfchand2@ncsu.edu>
2022-09-07 11:55:24 -0400

    Regular Expression, remove periods, store in list.

--- text_wrangling.py
+++ text_wrangling.py
@@ -19,7 +19,8 @@ class Text_Wrangling:
                 for line in file:
                     for word in enumerate(line.split()):
                         temp_num, temp_text = word #word is a Tuple. Separate the # from the Text.
-                        re_text = re.split(r"[,;]+", temp_text)
+                        re_text = re.split(r"[,;.]+", temp_text)
+                        ldoc_list.append( re_text[0] ) #Store the Text in list.
                         #Perhaps there's something useful with the # (number)
 
                 print('length of ldoc_list[] = %s' %str(len(ldoc_list)) ) #Print length of ldoc_list string
